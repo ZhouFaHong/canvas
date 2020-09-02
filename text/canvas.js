09d5a17414c33f8115dd200a6a5cb7e81b68283e
周发洪 <zhoufahong@itimebox.com>
2020-09-02 10:30:01 +0800

    修改文本画图

--- text/canvas.js
+++ text/canvas.js
@@ -39,12 +39,69 @@ function draw() {
 }
 
 
-// function draw() {
-//   var ctx = document.getElementById('canvas').getContext('2d');
-//   var text = ctx.measureText("foo"); // TextMetrics object
-//   text.width; // 16;
-//   console.log('width:'+text.width);
-// }
+function draw() {
+  var ctx = document.getElementById('canvas').getContext('2d');
+  var text = ctx.measureText("foo"); // TextMetrics object
+  text.width; // 16;
+  console.log('width:'+text.width);
+}
+
+function draw() {
+  var ctx = document.getElementById('canvas').getContext('2d');
+ 
+  canvas.width = 900;
+  canvas.height = 600;
+  canvas.style.border = "1px solid #000";
+
+  // =S 绘制坐标参照系
+  ctx.beginPath();
+  ctx.moveTo(0,301);
+  ctx.lineTo(900,301);
+  ctx.strokeStyle = "teal";			
+  ctx.stroke();		
+
+  ctx.moveTo(450,0);
+  ctx.lineTo(450,600);
+  ctx.stroke();
+
+  ctx.fillRect(445,295,10,10);
+  // =E 绘制坐标参照系
+
+  //绘制字体
+  ctx.font = '20px "微软雅黑"';
+  ctx.fillStyle = "red";
+  ctx.textBaseline = "top";
+  ctx.fillText("Top-g", 100, 300);
+
+  ctx.textBaseline = "middle";
+  ctx.fillText("middle-g", 200, 300);
+
+  ctx.textBaseline = "bottom";
+  ctx.fillText("bottom-g", 490, 300);
+  ctx.textBaseline = "hanging";
+  ctx.fillText("hanging", 490, 300);
+
+  ctx.textBaseline = "Alphabetic";
+  ctx.fillText("Alphabetic-g", 700, 300);
+
+
+  //绘制字体 设置对齐方式
+  ctx.fillStyle = "purple";
+  ctx.textAlign = "left";
+  ctx.strokeText("left", 450, 400);
+
+  ctx.textAlign = "right";
+  ctx.strokeText("right", 450, 440);
+
+  ctx.textAlign = "start";
+  ctx.strokeText("start", 450, 480);
+
+  ctx.textAlign = "end";
+  ctx.strokeText("end", 450, 520);
+
+  ctx.textAlign = "center";
+  ctx.strokeText("center", 450, 560);
+}
 
 draw();
 
